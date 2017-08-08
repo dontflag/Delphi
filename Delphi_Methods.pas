3a2bdf4e07de3b47465b24c388514b985d1934ef
dontflag <30798339+dontflag@users.noreply.github.com>
2017-08-08 17:00:49 +0300

    Update Delphi_Methods.pas

--- Delphi_Methods.pas
+++ Delphi_Methods.pas
@@ -18,8 +18,8 @@ implementation
 
 {$R *.dfm}
 
-procedure TSProject.RunInterpDebug; //первоначальное нажатие проводит только инициализацию
-var                                 //выполнение проводится после дальнейших действия пользователя
+procedure TSProject.RunInterpDebug; //first-time pressing makes only initialization of the interpreter
+var                                 //the execution proceeds after subsequent user actions
   i: integer;
   FileName: string;
   Execute: boolean;
@@ -27,28 +27,28 @@ var                                 //выполнение проводится после дальнейших дей
 begin
   SoftForm := TSoftForm(FForm);
   if SoftForm.ActiveSchema <> nil then begin
-    if TSSprutSchema(SoftForm.ActiveSchema).Method.IsSenderDeclar  then begin//схема класса
-      MessageDlg(IfLang('Debug is not available for a method declaration!','Отладка для объявления метода невозможна!'),mtInformation,[mbOk],0);
+    if TSSprutSchema(SoftForm.ActiveSchema).Method.IsSenderDeclar  then begin //class schema
+      MessageDlg(IfLang('Debug is not available for a method declaration!','РћС‚Р»Р°РґРєР° РґР»СЏ РѕР±СЉСЏРІР»РµРЅРёСЏ РјРµС‚РѕРґР° РЅРµРІРѕР·РјРѕР¶РЅР°!'),mtInformation,[mbOk],0);
       Exit;
     end;
   end else if SoftForm.TextMethod <> nil then
     if SoftForm.TextMethod.IsSenderDeclar then begin
-      MessageDlg(IfLang('Debug is not available for a method declaration!','Отладка для объявления метода невозможна!'),mtInformation,[mbOk],0);
+      MessageDlg(IfLang('Debug is not available for a method declaration!','РћС‚Р»Р°РґРєР° РґР»СЏ РѕР±СЉСЏРІР»РµРЅРёСЏ РјРµС‚РѕРґР° РЅРµРІРѕР·РјРѕР¶РЅР°!'),mtInformation,[mbOk],0);
       Exit;
     end;
-  DM.JvFldrDlg.Title := IfLang('Select a section to debug','Выберите отлаживаемый раздел');
-  if DM.JvFldrDlg.Directory = '' then  //directory запоминает последний открытый путь в рамках текущего сеанса программы
+  DM.JvFldrDlg.Title := IfLang('Select a section to debug','Р’С‹Р±РµСЂРёС‚Рµ РѕС‚Р»Р°Р¶РёРІР°РµРјС‹Р№ СЂР°Р·РґРµР»');
+  if DM.JvFldrDlg.Directory = '' then  //directory stores last opened path due the current program session
     DM.JvFldrDlg.Directory := ExtractFilePath(ExtractFileDir(ParamStr(0))) + 'Projects\';
   Execute := DM.jvFldrDlg.Execute;
   if Execute then
     FileName := DM.JvFldrDlg.Directory;
   if FileName = '' then begin
     if Execute then
-      MessageDlg(IfLang('Section file not found!','Файл раздела не найден!'),mtError,[mbOk],0);
+      MessageDlg(IfLang('Section file not found!','Р¤Р°Р№Р» СЂР°Р·РґРµР»Р° РЅРµ РЅР°Р№РґРµРЅ!'),mtError,[mbOk],0);
     Exit;
   end;
-  //FileName := SectionName; //получим имя раздела, если один и тот же объект TSMethod задействован в двух разделах
-  InterpLib := LoadLibrary('Interpreter.dll'); //просто проверяем соединение с интерпретатором
+  //FileName := SectionName; //get current section name, if the same TSMethod object is used in different sections
+  InterpLib := LoadLibrary('Interpreter.dll'); //check connection with interpreter
   InitFunc := GetProcAddress(InterpLib, PChar('Init'));
   RunProc := GetProcAddress(InterpLib, PChar('Run'));
   StopProc := GetProcAddress(InterpLib, PChar('Stop'));
@@ -57,11 +57,11 @@ begin
     try
       Globals := TStringList.Create;
       Locals := TStringList.Create;
-      FillGlobals; //создадим и заполним списки
+      FillGlobals; //create and fill variable lists
       FillLocals;
-      for i:=0 to Globals.Count-1 do begin  //создадим и заполним базовые значения в списках
+      for i:=0 to Globals.Count-1 do begin  //create and fill default values  in lists
         if Globals.Objects[i] <> nil then
-          if Globals.Objects[i] is TSParam then //у параметра нету поля Value
+          if Globals.Objects[i] is TSParam then //parameter of the method has no Value field
             TSCustomObject(Globals.Objects[i]).DebugValue := TSParam(Globals.Objects[i]).DefValue
           else
             TSCustomObject(Globals.Objects[i]).DebugValue := TSObject(Globals.Objects[i]).Value;
@@ -79,18 +79,18 @@ begin
           DebugMethod := TSSprutSchema(ActiveSchema).Method
         else if TextMethod <> nil then
           DebugMethod := TextMethod;
-      //Debug Vertex при инициализации не определяем, пока не появится необходимость обратного
+      //Debug Vertex is not going to be set due initialization, until it will become necessary
       InterpDebugPause := True;
     except
       on E : Exception do
-        MessageDlg(IfLang('Error during calling Init: ', 'Ошибка при вызове функции Init: ')+E.Message, mtWarning, [mbOK], 0);
+        MessageDlg(IfLang('Error during calling Init: ', 'РћС€РёР±РєР° РІС‹Р·РѕРІР° С„СѓРЅРєС†РёРё Init: ')+E.Message, mtWarning, [mbOK], 0);
     end
   else begin
-    MessageDlg(IfLang('It is not possible to connect to interpreter.', 'Не удаётся установить связь с интерпретатором'), mtWarning, [mbOK], 0);
+    MessageDlg(IfLang('It is not possible to connect to interpreter.', 'РќРµ СѓРґР°С‘С‚СЃСЏ СѓСЃС‚Р°РЅРѕРІРёС‚СЊ СЃРІСЏР·СЊ СЃ РёРЅС‚РµСЂРїСЂРµС‚Р°С‚РѕСЂРѕРј'), mtWarning, [mbOK], 0);
     Exit;
   end;
-  FDEditValueFr := TEditValueFr.Create(nil); //временно
-  FForm.Caption := FormCaption + IfLang(' [Running]',' [Выполнение]');
+  FDEditValueFr := TEditValueFr.Create(nil); //temporary
+  FForm.Caption := FormCaption + IfLang(' [Running]',' [Р’С‹РїРѕР»РЅРµРЅРёРµ]');
   TSoftForm(FForm).DebugSB.Panels[6].Text := DM.JvFldrDlg.DisplayName;
 end;
 
@@ -100,7 +100,7 @@ var
 begin
   if not InterpDebug then
     Exit;
-  {for i:=0 to Globals.Count-1 do    //очищение DebugValue происходит в интерпретаторе
+  {for i:=0 to Globals.Count-1 do    //DebugValue clears in the interpreter
     TSCustomObject(Globals.Objects[i]).DebugValue := '';
   for i:=0 to Locals.Count-1 do
     try
@@ -108,17 +108,17 @@ begin
     except
       on EInvalidPointer do ShowMessage('Invalid pointer!');
     end; }
-  FNextLine := 'Stop'; //20.04.17 когда будет сохранение состояний программы - сервер сам будет поле заполнять
+  FNextLine := 'Stop'; //20.04.17 when the program save is occured - server will fill this field
   CallStopProc;//Stop
   DebugVertex := nil;
   DebugMethod := nil;
-  FDEditValueFr.Hide; //временно
+  FDEditValueFr.Hide; //temporary
   FDEditValueFr.Free;
   FForm.Caption := FormCaption;
- { FreeAndNil(Globals); //объекты остаются в памяти, но удалять их и не надо, тк это объекты объектной модели программы
-  FreeAndNil(Locals);} //списки очищаются в интерпретаторе
+ { FreeAndNil(Globals); //The objects are saved in memory, but you dont have to delete them, because these are the objects of object model of the soft editor
+  FreeAndNil(Locals);} //lists clears in the interpreter
   FreeLibrary(InterpLib);
-  FNextLine := ''; //см. 'Stop'
+  FNextLine := ''; 
 end;
 
 procedure TSProject.CallRunProc(Mode: TRunMode);
@@ -128,10 +128,10 @@ var
 begin
   BPList := TStringList.Create;
   FOldDebugTime := FDebugTime;
-  for i:= 0 to BreakpointsCount-1 do //ID для графики - VertexID, для текста - Line
+  for i:= 0 to BreakpointsCount-1 do //for graphic bp - VertexID, for text - Line
     BPList.Add(IntToStr(TSBreakpoint(FBreakpoints[i]).Method.MetID)+'.'+IntToStr(TSBreakpoint(FBreakpoints[i]).ID));
   if not InterpDebug then
-    MessageDlg(IfLang('It is not possible to connect to interpreter', 'Не удаётся установить связь с интерпретатором'), mtWarning, [mbOK], 0)
+    MessageDlg(IfLang('It is not possible to connect to interpreter', 'РќРµ СѓРґР°С‘С‚СЃСЏ СѓСЃС‚Р°РЅРѕРІРёС‚СЊ СЃРІСЏР·СЊ СЃ РёРЅС‚РµСЂРїСЂРµС‚Р°С‚РѕСЂРѕРј!'), mtWarning, [mbOK], 0)
   else begin
     InterpDebugPause := False;
     FNextLine := '';
@@ -154,11 +154,11 @@ begin
     DebugLine := 0;
     if DebugMethod.Schema <> nil then begin
       for i:=0 to DebugMethod.Schema.Count-1 do
-        if Length(NextLine)>0 then //пока что там либо 0.0, либо адрес, либо ''
+        if Length(NextLine)>0 then //there is '0.0' or adress or ''
           if TSVertex(DebugMethod.Schema[i]).VertexID = StrToInt(copy(NextLine,pos('.',NextLine)+1,Length(NextLine))) then
             DebugVertex := TSCustomSprutAction(DebugMethod.Schema[i]);
     end else if DebugMethod.Text[DebugMethod.TextLang] <> nil then
-      if Length(NextLine)>0 then //пока что там либо 0.0, либо адрес, либо ''
+      if Length(NextLine)>0 then //there is '0.0' or adress or ''
         DebugLine := StrToInt(copy(NextLine,pos('.',NextLine)+1,Length(NextLine)))-1;
     DebugSB.Panels[cTaktPanel].Text := IntToStr(FDebugTime);
   end;
@@ -168,7 +168,7 @@ begin
     UpdateDebugWindows;
     Exit;
   end;
-  InterpDebugPause := True; //прогон завершён, на бп встали => выполнение приостановлено
+  InterpDebugPause := True; //running is over, the program is on the breakpoint => execution is paused
   UpdateDebugWindows;
 end;
 
@@ -176,9 +176,9 @@ procedure TSProject.CallStopProc;
 var
   i: integer;
 begin
-  Stop(FNextLine,FDebugTime); //пауза
+  Stop(FNextLine,FDebugTime); //pause
   //MessageDlg('NextLine: ' +FNextLine + '; DebugTime: ' +IntToStr(FDebugTime), mtInformation, [mbOk],0);
-  for i:=0 to BreakpointsCount-1 do //вдруг брекпойнт на первой "строке"
+  for i:=0 to BreakpointsCount-1 do //if breakpoint is on the first line
     if FNextLine = IntToStr(TSBreakpoint(FBreakpoints[i]).Method.MetID)+'.'+IntToStr(TSBreakpoint(FBreakpoints[i]).ID) then
       with TSBreakpoint(Breakpoints[i]) do begin
         PassIndex := PassIndex+1;
